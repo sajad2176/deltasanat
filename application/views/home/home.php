92a7c8c577a61b056904833e69febe7e600c8ae2
novinhub <48179603+novinhub@users.noreply.github.com>
2019-03-27 01:42:45 +0430

    home

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,17 +1,15 @@
 <!-- Dashboard content -->
-<div class="row">
+<div  class="row">
 	<div class="col-lg-8">
 		<div class="row">
 			<div class="col-lg-4">
-
 				<!-- Members online -->
 				<div class="panel bg-teal-400">
 					<div class="panel-body">
 						<div class="heading-elements">
-							<span class="heading-text badge bg-teal-800">+53,6%</span>
+							<span class="heading-text badge bg-teal-800">هماهنگی</span>
 						</div>
-
-						<h3 class="no-margin">3,450</h3> sd شده
+						<h3 class="no-margin">3,450</h3> 
 						<div class="text-muted text-size-small">489 avg</div>
 					</div>
 
@@ -20,36 +18,17 @@
 					</div>
 				</div>
 				<!-- /members online -->
-
 			</div>
-
 			<div class="col-lg-4">
-
 				<!-- Current server load -->
 				<div class="panel bg-pink-400">
 					<div class="panel-body">
 						<div class="heading-elements">
-							<ul class="icons-list">
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-cog3"></i> <span class="caret"></span></a>
-									<ul class="dropdown-menu dropdown-menu-right">
-										<li><a href="#"><i class="icon-sync"></i> Update data</a>
-										</li>
-										<li><a href="#"><i class="icon-list-unordered"></i> Detailed log</a>
-										</li>
-										<li><a href="#"><i class="icon-pie5"></i> Statistics</a>
-										</li>
-										<li><a href="#"><i class="icon-cross3"></i> Clear list</a>
-										</li>
-									</ul>
-								</li>
-							</ul>
+							<span class="heading-text badge bg-success-800">هماهنگی</span>
 						</div>
-
-						<h3 class="no-margin">49.4%</h3> Current server load
+						<h3 class="no-margin">49.4%</h3> 
 						<div class="text-muted text-size-small">34.6% avg</div>
 					</div>
-
 					<div class="chart" id="server-load"></div>
 				</div>
 				<!-- /current server load -->
@@ -57,202 +36,134 @@
 			</div>
 
 			<div class="col-lg-4">
-
 				<!-- Today's revenue -->
 				<div class="panel bg-blue-400">
 					<div class="panel-body">
 						<div class="heading-elements">
-							<ul class="icons-list">
-								<li><a data-action="reload"></a>
-								</li>
-							</ul>
+							<span class="heading-text badge bg-blue-800">هماهنگی</span>
 						</div>
-
-						<h3 class="no-margin">$18,390</h3> Today's revenue
+						<h3 class="no-margin">$18,390</h3> 
 						<div class="text-muted text-size-small">$37,578 avg</div>
 					</div>
-
 					<div class="chart" id="today-revenue"></div>
 				</div>
 				<!-- /today's revenue -->
-
 			</div>
 		</div>
 		<!-- Marketing campaigns -->
 		<div class="panel panel-flat">
 			<div class="panel-body">
-					<div class="panel-heading">
-			<h5 class="panel-title">مجموع معاملات</h5>
-
-		</div>
-				<img src="<?php echo base_url('files/');?>/assets/img/Annotation 2019-03-13 161608.jpg" </div>
-
+				<div class="panel-heading">
+					<h5 class="panel-title">مجموع معاملات</h5>
+				</div>
+				<img style="width: 100%" src="<?php echo base_url('files/');?>/assets/img/Annotation 2019-03-13 161608.jpg" </div>
 			</div>
 			<!-- /marketing campaigns -->
-
-
 			<!-- Quick stats boxes -->
-
 			<!-- /quick stats boxes -->
-
-
-
-
 		</div>
 	</div>
 
 	<div class="col-lg-4">
-
-		<!-- Progress counters -->
-		<div class="row">
-			<div class="col-md-6">
-
-			</div>
-
-			<div class="col-md-6">
-
-
-
-			</div>
-		</div>
-		<!-- /progress counters -->
-
-
 		<!-- Daily sales -->
 		<div class="panel panel-flat">
 			<div class="panel-body">
 				<div class="panel-heading">
-
 					<h6 class="panel-title">خرید و فروش ارز ها</h6>
 					<div class="heading-elements">
 						<span class="heading-text">Balance: <span class="text-bold text-danger-600 position-right">$4,378</span></span>
 
 					</div>
 				</div>
-
-				<div class="panel-body">
-					<div class="chart" id="sales-heatmap"></div>
-				</div>
+				
 
 				<div class="table-responsive">
 					<table class="table text-nowrap">
 						<thead>
 							<tr>
-								<th>ارز</th>
-								<th>خرید</th>
-								<th>فروش</th>
+								<th style="width: 85px">ارز</th>
+								<th class="text-warning-600" style="padding-right: 30px">خرید</th>
+								<th class="text-primary">فروش</th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
 								<td>
-									<div class="media-left media-middle">
-										<a href="#" class="btn bg-primary-400 btn-rounded btn-icon btn-xs">
-															<span class="letter-icon"></span>
-														</a>
-									
-
-
-									</div>
-
 									<div class="media-body">
 										<div class="media-heading">
-											<a href="#" class="letter-icon-title">دلار</a>
+											<h5 class="letter-icon-title">دلار</h5>
 										</div>
-
-										<div class="text-muted text-size-small"><i class="icon-checkmark3 text-size-mini position-left"></i> New order</div>
 									</div>
 								</td>
 								<td>
-									<span class="text-muted text-size-small"><?php echo number_format($buy_dollar->cd + $buy_dollar->wd);?></span>
+									<h6 class="text-semibold text-warning-600 pr-34 no-margin ">
+										<?php echo number_format($buy_dollar->cd + $buy_dollar->wd);?>
+									</h6>
 								</td>
 								<td>
-									<h6 class="text-semibold no-margin"><?php echo number_format($sell_dollar->cd + $sell_dollar->wd); ?></h6>
+									<h6 class="text-semibold text-primary pr-8 no-margin">
+										<?php echo number_format($sell_dollar->cd + $sell_dollar->wd); ?>
+									</h6>
 								</td>
 							</tr>
-
 							<tr>
-
 							</tr>
-
 							<tr>
 								<td>
-									<div class="media-left media-middle">
-										<a href="#" class="btn bg-indigo-400 btn-rounded btn-icon btn-xs">
-															<span class="letter-icon"></span>
-														</a>
-									
-
-
-									</div>
-
+								
 									<div class="media-body">
 										<div class="media-heading">
-											<a href="#" class="letter-icon-title">یوان</a>
+											<h5  class="letter-icon-title">یوان</h5>
 										</div>
-
-										<div class="text-muted text-size-small"><i class="icon-lifebuoy text-size-mini position-left"></i> Support</div>
 									</div>
 								</td>
 								<td>
-									<span class="text-muted text-size-small"><?php echo number_format($buy_yuan->cd + $buy_yuan->wd); ?></span>
+									<h6 class=" text-semibold text-warning-600 pr-34 no-margin">
+										<?php echo number_format($buy_yuan->cd + $buy_yuan->wd); ?>
+									</h6>
 								</td>
 								<td>
-									<h6 class="text-semibold no-margin"><?php echo number_format($sell_yuan->cd + $sell_yuan->wd); ?></h6>
+									<h6 class="text-semibold text-primary  pr-8 no-margin">
+										<?php echo number_format($sell_yuan->cd + $sell_yuan->wd); ?>
+									</h6>
 								</td>
 							</tr>
-
 							<tr>
 								<td>
-									<div class="media-left media-middle">
-										<a href="#" class="btn bg-success-400 btn-rounded btn-icon btn-xs">
-															<span class="letter-icon"></span>
-														</a>
-									
-
-
-									</div>
-
 									<div class="media-body">
 										<div class="media-heading">
-											<a href="#" class="letter-icon-title">یورو</a>
-										</div>
-
-										<div class="text-muted text-size-small"><i class="icon-lifebuoy text-size-mini position-left"></i> Support</div>
+											<h5 class="letter-icon-title">یورو</h5>
+										</div>	
 									</div>
 								</td>
 								<td>
-									<span class="text-muted text-size-small"><?php echo number_format($buy_euro->cd + $buy_euro->wd); ?></span>
+									<h6 class=" text-semibold text-warning-600 pr-34 no-margin">
+										<?php echo number_format($buy_euro->cd + $buy_euro->wd); ?>
+									</h6>
 								</td>
 								<td>
-									<h6 class="text-semibold no-margin"><?php echo number_format($sell_euro->cd + $sell_euro->wd); ?></h6>
+									<h6 class="text-semibold text-primary pr-8 no-margin">
+										<?php echo number_format($sell_euro->cd + $sell_euro->wd); ?>
+									</h6>
 								</td>
 							</tr>
-
 							<tr>
 								<td>
-									<div class="media-left media-middle">
-										<a href="#" class="btn bg-danger-400 btn-rounded btn-icon btn-xs">
-															<span class="letter-icon"></span>
-														</a>
-									
-
-
-									</div>
-
 									<div class="media-body">
 										<div class="media-heading">
-											<a href="#" class="letter-icon-title">درهم</a>
+											<h5 class="letter-icon-title">درهم</h5>
 										</div>
-
-										<div class="text-muted text-size-small"><i class="icon-spinner11 text-size-mini position-left"></i> Renewal</div>
 									</div>
 								</td>
 								<td>
-									<span class="text-muted text-size-small"><?php echo number_format($buy_derham->cd + $buy_derham->wd); ?></span>
+									<h6 class="text-semibold text-warning-600 pr-34 no-margin">
+										<?php echo number_format($buy_derham->cd + $buy_derham->wd); ?>
+									</h6>
 								</td>
 								<td>
-									<h6 class="text-semibold no-margin"><?php echo number_format($sell_derham->cd + $sell_derham->wd); ?></h6>
+									<h6 class="text-semibold text-primary pr-8 no-margin">
+										<?php echo number_format($sell_derham->cd + $sell_derham->wd); ?>
+									</h6>
 								</td>
 							</tr>
 						</tbody>
@@ -263,110 +174,35 @@
 		<!-- /daily sales -->
 		<div class="panel panel-flat">
 			<div class="panel-body">
-<!-- Members online -->	<div class="panel-heading">
-			<h5 class="panel-title">موجودی</h5>
-
-		</div>
+				<!-- Members online -->
+				<div class="panel-heading">
+					<h5 class="panel-title">موجودی</h5>
+				</div>
 				<div class="row">
-				<?php foreach($remain as $remains){ ?>
-				<div class="col-md-6">
-				<div class="panel bg-teal-400">
-					<div class="panel-body">
-						<h3 class="no-margin"><?php echo $remains->amount_unit;?></h3>
-						<div class="text-muted text-size-small"><?php echo $remains->name;?></div>
-					</div>
-
-					<div class="container-fluid">
-						<div class="chart" id="members-online"></div>
-					</div>
-				</div>
-				</div>
-				<!-- /members online -->
-				<?php } ?>
-			
-
-			<div class="col-lg-6">
-
-				<!-- Current server load -->
-				<div class="panel bg-pink-400">
-					<div class="panel-body">
-						<div class="heading-elements">
-							<ul class="icons-list">
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-cog3"></i> <span class="caret"></span></a>
-									<ul class="dropdown-menu dropdown-menu-right">
-										<li><a href="#"><i class="icon-sync"></i> Update data</a>
-										</li>
-										<li><a href="#"><i class="icon-list-unordered"></i> Detailed log</a>
-										</li>
-										<li><a href="#"><i class="icon-pie5"></i> Statistics</a>
-										</li>
-										<li><a href="#"><i class="icon-cross3"></i> Clear list</a>
-										</li>
-									</ul>
-								</li>
-							</ul>
+					<?php foreach($remain as $remains){ ?>
+					<div class="col-md-6">
+						<div class="panel bg-teal-400">
+							<div class="panel-body">
+								<h3 class="no-margin">
+									<?php echo $remains->amount_unit;?>
+								</h3>
+								<div class="text-muted text-size-small">
+									<?php echo $remains->name;?>
+								</div>
+							</div>
+							<div class="container-fluid">
+								<div class="chart" id="members-online"></div>
+							</div>
 						</div>
-
-						<h3 class="no-margin">49.4%</h3> Current server load
-						<div class="text-muted text-size-small">34.6% avg</div>
 					</div>
-
-					<div class="chart" id="server-load"></div>
-				</div>
-				<!-- /current server load -->
-
-			</div>
-</div>
-			<div class="row">
-			<div class="col-lg-6">
-
-				<!-- Today's revenue -->
-				<div class="panel bg-blue-400">
-					<div class="panel-body">
-						<div class="heading-elements">
-							<ul class="icons-list">
-								<li><a data-action="reload"></a>
-								</li>
-							</ul>
-						</div>
-
-						<h3 class="no-margin">$18,390</h3> Today's revenue
-						<div class="text-muted text-size-small">$37,578 avg</div>
+					<!-- /members online -->
+					<?php } ?>
+					<div class="col-lg-6">
 					</div>
-
-					<div class="chart" id="today-revenue"></div>
 				</div>
-				<!-- /today's revenue -->
-
-			
 			</div>
-	<div class="col-lg-6">
-
-				<!-- Today's revenue -->
-				<div class="panel bg-blue-400">
-					<div class="panel-body">
-						<div class="heading-elements">
-							<ul class="icons-list">
-								<li><a data-action="reload"></a>
-								</li>
-							</ul>
-						</div>
-
-						<h3 class="no-margin">$18,390</h3> Today's revenue
-						<div class="text-muted text-size-small">$37,578 avg</div>
-					</div>
-
-					<div class="chart" id="today-revenue"></div>
-				</div>
-				<!-- /today's revenue -->
-
-			
-			</div>
-
 		</div>
-	</div>
 
-</div>
+	</div>
 </div>
 <!-- /dashboard content -->
